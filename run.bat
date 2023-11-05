bb5460aea5dcedc9b82ebfe8e8f664b58c5a1170
lykhorovych <oleglihorovich@gmail.com>
2023-11-05 19:57:27 +0200

    add run.bat and become_qa_auto files

---
+++ run.bat
@@ -0,0 +1,7 @@
+pytest tests -m rztk -v -s
+pytest tests -m nova_poshta -v -s
+pytest tests -m amazon -v -s
+pytest tests -m ui -v -s
+pytest tests -m database -v -s
+pytest tests -m api -v -s
+pytest tests -m http -v -s
