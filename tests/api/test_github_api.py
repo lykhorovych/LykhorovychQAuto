24f303c4a4cdda259ba4fb603d4874d58823c97d
lykhorovych <oleglihorovich@gmail.com>
2023-12-08 21:25:41 +0200

    add run_github.yaml

--- tests/api/test_github_api.py
+++ tests/api/test_github_api.py
@@ -19,7 +19,7 @@ def test_user_not_exists(github_api):
 def test_repo_can_be_found(github_api):
     r = github_api.search_repo("become-qa-auto")
 
-    assert r["total_count"] == 50
+    assert r["total_count"] == 54
     assert "become-qa-auto" in r["items"][0]["name"]
 
 
